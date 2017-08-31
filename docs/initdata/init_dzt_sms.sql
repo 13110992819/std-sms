29e59828258cbb518430b03aa0937e39cf3c58e8
pyez1158 <pyez1158@163.com>
2017-08-31 11:10:03 +0800

    脚本整理

---
+++ docs/initdata/init_dzt_sms.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT company_code,channel,k,v,remark FROM std_sms.tjc_configure where company_code='CD-CDZT000009'
+-- Date: 2017-08-31 11:08
+*/
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_account_1','hh8085','account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_password_1','sh8085','password');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_userid_1','178','userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_userid_2','178','userid');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_account_2','hh8085','account');
+INSERT INTO `tjc_configure` (`company_code`,`channel`,`k`,`v`,`remark`) VALUES ('CD-CDZT000009','HHXX','hhxx_password_2','sh8085','password');
